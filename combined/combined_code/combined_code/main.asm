acc57e3b864a73dffe4c8efd6cd17225e2fa3dd7
Oneplus-2997 <mario808@hotmail.com>
2024-03-12 04:33:37 +0530

    added positions to combined code

--- combined/combined_code/combined_code/main.asm
+++ combined/combined_code/combined_code/main.asm
@@ -6,7 +6,32 @@
 ;
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+.include "m328pdef.inc"
+rotate_pos:.DB 40,70,92,110,180,110,90,70
+
+.cseg
+.org	0x00
+
+setup:
+    LDI R16, 0b10000010
+    OUT DDRB, R16
+
+again:     
+    LDI   R26, 8          ;counter for # of rotation pos
+    LDI   ZL, LOW(rotate_pos)
+    LDI   ZH, HIGH(rotate_pos)
+
+rotate: 
+    LPM   R24, Z+         ;load rotation pos
+    RCALL rotate_servo    ;& rotate servo
+
+loop:
+
+
+
+
+    DEC   R26
+    BRNE  rotate              ;go back & get another rotate pos
+    ;-----------------------------------------------------------
+    RJMP  again           ;go back & repeat
+;--------------------------------------------
